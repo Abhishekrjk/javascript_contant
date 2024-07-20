f7a272f4fc447f10ff138113f080499d7a48a3e2
F.S.dev-abhi.rjk <abhishekrjk789@gmail.com>
2024-07-20 15:48:56 +0530

    Add files via upload

---
+++ 26js.js
@@ -0,0 +1,19 @@
+var num,c=0,a;
+for( a=2;a<num;a++){
+ if(num%a==0)
+ {
+     c++;
+ }  
+ }
+ 
+ if(c==0)
+ {
+  document.write("number is prime ");
+ }
+ else
+ {
+  document.write("number is not prime number");
+ }
+
+
+
